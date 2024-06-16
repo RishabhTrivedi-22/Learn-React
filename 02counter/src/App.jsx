fda5765baa078a74703536e22670b78822730045
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-06-16 18:25:15 +0530

    useState hook used

--- 02counter/src/App.jsx
+++ 02counter/src/App.jsx
@@ -1,19 +1,34 @@
-
+import {useState} from 'react'
 import './App.css'
 
 function App() {
 
-  let count = 0
+  let [count, setCount] = useState(0)
   const addCount = () => {
-    console.log('Clicked', Math.random())
+    if (count == 20){
+      setCount(20)
+    }
+    else {
+      setCount(count + 1)
+    }
+  }
+
+  const removeCount = () => {
+    if(count == 0){
+      setCount(0)
+    }
+    else {
+      setCount(count - 1)
+    }
   }
   return (
     <>
       <h1>Vite React Counter Project</h1>
       <h2>Counter Value : {count}</h2>
+      <p>{count}</p>
       <br />
-      <button onClick={addCount}>Increase</button>
-      <button>Decrease</button>
+      <button onClick={addCount}>Increase {count}</button>
+      <button onClick={removeCount}>Decrease {count}</button>
     </>
   )
 }
