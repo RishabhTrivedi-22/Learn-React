4802c37457e2a7c0e2c614a2837725a1d2a8bad2
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-06-27 17:53:48 +0530

    07 React Router

---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-lg bg-green-500 p-4'>React Router</h1>
+    </>
+  )
+}
+
+export default App
