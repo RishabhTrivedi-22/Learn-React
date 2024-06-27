2b04e8163c2693cf65a280a03d45d95ec0ca3432
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-06-27 16:24:40 +0530

    06 Proj Currency converter

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from 'react'
+
 function App() {
 
   return (
     <>
-       <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     <h1 className='text-3xl text-center underline bg-orange-500'>Currency Converter</h1>
     </>
   )
 }
