68b59cb3bde7140c32c5f165f27e6a9b74f41496
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-06-28 11:31:09 +0530

    Basic Routing Complete.

---
+++ 07reactrouter/src/components/index.js
@@ -0,0 +1,7 @@
+import Header from './Header/Header.jsx';
+import Footer from './Footer/Footer.jsx';
+import Home from './Home/Home.jsx';
+import About from './About/About.jsx';
+
+
+export { Header, Footer, Home, About };
--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -1,10 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import Layout from './Layout.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import { About, Home } from './components/index.js';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />
+      },
+      {
+        path: 'about',
+        element: <About />
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
