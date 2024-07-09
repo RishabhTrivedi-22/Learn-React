693a7425d00d76720e6a87cad78e447298d144d3
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-07-09 17:14:45 +0530

    Context API part -1 fin

--- 08miniContext/src/App.jsx
+++ 08miniContext/src/App.jsx
@@ -1,13 +1,19 @@
-import { useState } from 'react'
 import './App.css'
+import { useState } from 'react';
+import UserContextProvider from './context/UserContextProvider';
+import Login from './components/Login';
+import Profile from './components/Profile';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <UserContextProvider>
       <h1>React with Chai</h1>
-    </>
+      <br />
+      <Login />
+      <Profile />
+    </UserContextProvider>
   )
 }
 
--- 08miniContext/src/context/UserContextProvider.jsx
+++ 08miniContext/src/context/UserContextProvider.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import UserContext from "./userContext";
 
-const UserContextProvider = ({children}) => {
+const UserContextProvider = ({ children }) => {
   const [user, setUser] = React.useState(null);
 
   return (
-    <UserContext.Provider value={{user, setUser}}>
+    <UserContext.Provider value={{ user, setUser }}>
       {children}
     </UserContext.Provider>
-  )
+  );
 };
 
 export default UserContextProvider;
