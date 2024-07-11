105d1989669378ffe47544dca233a2b9771eaeca
RishabhTrivedi-22 <rishabhtrivedi695@gmail.com>
2024-07-11 16:47:06 +0530

    TODO using context API finished

--- 10todo/src/App.jsx
+++ 10todo/src/App.jsx
@@ -1,38 +1,66 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { TodoProvider } from "./contexts";
 import "./App.css";
+import { TodoForm, TodoItem } from "./components";
 
 function App() {
-  const [todos, setTodos] = useState([])
+  const [todos, setTodos] = useState([]);
 
   const addTask = (taskItem) => {
-    setTodos((prev) => [...prev, {id: Date.now(), ...taskItem}])
-  }
+    setTodos((prev) => [...prev, { id: Date.now(), ...taskItem }]);
+  };
 
   const updateTask = (id, taskItem) => {
-    setTodos((prev) => prev.map((prevTask) => (prevTask.id === id ? taskItem : prevTask)))
-  }
+    setTodos((prev) =>
+      prev.map((prevTask) => (prevTask.id === id ? taskItem : prevTask))
+    );
+  };
 
   const deleteTasks = (id) => {
-    setTodos((prev) => prev.filter((prevTask) => prevTask.id !== id))
-  }
+    setTodos((prev) => prev.filter((prevTask) => prevTask.id !== id));
+  };
 
   const toggleStatus = (id) => {
-    setTodos((prev) => prev.map((prevTask) => prevTask.id === id ? {...prevTask, completeStatus: !prevTask.completeStatus} : prevTask))
-  }
+    setTodos((prev) =>
+      prev.map((prevTask) =>
+        prevTask.id === id
+          ? { ...prevTask, completeStatus: !prevTask.completeStatus }
+          : prevTask
+      )
+    );
+  };
+
+  useEffect(() => {
+    const todoList = JSON.parse(localStorage.getItem("todos"));
+
+    if(todoList && todoList.length > 0) {
+      setTodos(todoList)
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem('todos', JSON.stringify(todos))
+  }, [todos])
 
   return (
-    <TodoProvider value={{todos, addTask, updateTask, deleteTasks, toggleStatus}}>
+    <TodoProvider
+      value={{ todos, addTask, updateTask, deleteTasks, toggleStatus }}
+    >
       <div className="bg-[#172842] min-h-screen py-8">
         <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
           <h1 className="text-2xl font-bold text-center mb-8 mt-2">
             Manage Your Todos
           </h1>
           <div className="mb-4">
-            {/* Todo form goes here */}
+            <TodoForm />
           </div>
           <div className="flex flex-wrap gap-y-3">
-            {/*Loop and Add TodoItem here */}
+            {todos.map((todo) => (
+              <div key={todo.id}
+                  className="w-full">
+                    <TodoItem todo={todo}/>
+              </div>
+            ))}
           </div>
         </div>
       </div>
